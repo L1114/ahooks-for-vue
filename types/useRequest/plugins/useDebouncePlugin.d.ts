c9fd468c8ed5436eb4bd1c5cef88449b15b40c07
郑磊 <zlei0885@163.com>
2024-03-09 23:56:02 +0800

    0.1.1-beta.7

---
+++ types/useRequest/plugins/useDebouncePlugin.d.ts
@@ -0,0 +1,3 @@
+import { Plugin } from "../type";
+declare const useDebouncePlugin: Plugin<any, any[]>;
+export default useDebouncePlugin;
---
+++ types/useRequest/plugins/useThrottlePlugin.d.ts
@@ -0,0 +1,3 @@
+import { Plugin } from "../type";
+declare const useThrottlePlugin: Plugin<any, any[]>;
+export default useThrottlePlugin;
