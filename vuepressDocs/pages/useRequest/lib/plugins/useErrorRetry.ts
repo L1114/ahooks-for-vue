70dccceef8b85b0adc18a8c234ada179e52d0dda
郑磊 <zlei0885@163.com>
2024-03-16 23:51:41 +0800

    feat: useErrorRetry默认间隔时间

--- vuepressDocs/pages/useRequest/lib/plugins/useErrorRetry.ts
+++ vuepressDocs/pages/useRequest/lib/plugins/useErrorRetry.ts
@@ -1,42 +1,67 @@
 import { Plugin } from "../type";
 // import type Fetch from "./Fetch";
-import { watch, computed } from "vue-demi";
+import { watch, isRef, ref } from "vue-demi";
 
 import { toRawData } from "../../../utils/index";
 
 const useErrorRetry: Plugin<any, any[]> = (
   fetchInstance,
-  { retryCount: _retryCount = 0, retryInterval: _retryInterval = 2000 }
+  { retryCount: _retryCount = 0, retryInterval: _retryInterval }
 ) => {
-  let executedTryCount = 0;
+  const executedTryCount = ref(0);
   let hasError = false;
+
+  const getRetryInterval = () => {
+    let value = toRawData(_retryInterval);
+    value =
+      value >= 0
+        ? value
+        : Math.min(30 * 1000, Math.pow(2, executedTryCount.value + 1) * 1000);
+    return value;
+  };
+
   let retryCount = toRawData(_retryCount);
-  let retryInterval = toRawData(_retryInterval);
+  let retryInterval = getRetryInterval();
   const getEnableTry = () => Number(retryCount) > 0 || retryCount === -1;
   let enableTry = getEnableTry();
   let timer: ReturnType<typeof setTimeout>;
 
-  watch([_retryCount, _retryInterval], () => {
-    retryCount = toRawData(_retryCount);
-    retryInterval = toRawData(_retryInterval);
-  });
+  watch(
+    [_retryCount, executedTryCount].filter((e: any) => isRef(e)),
+    () => {
+      retryCount = toRawData(_retryCount);
+      enableTry = getEnableTry();
+      retryInterval = getRetryInterval();
+      if (!enableTry) {
+        clearTimeout(timer);
+      }
+    }
+  );
+  watch(
+    [_retryInterval].filter((e: any) => isRef(e)),
+    () => {
+      executedTryCount.value = 0;
+      retryInterval = getRetryInterval();
+    }
+  );
   return {
     onRequest: () => {
       if (hasError && enableTry) {
-        executedTryCount++;
-        console.log(`错误重试:第${executedTryCount}次`);
+        executedTryCount.value++;
+        console.log(`错误重试:第${executedTryCount.value}次`);
       }
       clearTimeout(timer);
       return {};
     },
     onSuccess: () => {
-      executedTryCount = 0;
+      executedTryCount.value = 0;
       hasError = false;
     },
     onError: () => {
       hasError = true;
       if (enableTry) {
-        if (executedTryCount < retryCount || retryCount === -1) {
+        if (executedTryCount.value < retryCount || retryCount === -1) {
+          console.log("retryInterval :>> ", retryInterval);
           clearTimeout(timer);
           // enableTry = false;
           timer = setTimeout(() => {
--- vuepressDocs/pages/useRequest/retry/demo.vue
+++ vuepressDocs/pages/useRequest/retry/demo.vue
@@ -3,17 +3,23 @@ import { mockFetchError } from "@/api";
 import { useRequest } from "ahooks-for-vue";
 import { ref } from "vue";
 
-const retryCount = 6;
+const retryCount = ref(100);
+const retryInterval = ref(2000);
 const { loading, data, run } = useRequest(mockFetchError, {
   retryCount,
-  // throttleTrailing: false,
+  retryInterval,
 });
 </script>
 <template>
   <div>
     <button type="button" @click="run">run</button>
 
-    <button type="button" @click="() => (retryCount = 100)">set</button>
+    <button type="button" @click="() => (retryCount = 100)">
+      set retryCount
+    </button>
+    <button type="button" @click="() => (retryInterval = -1)">
+      set retryInterval
+    </button>
     <br />
     data:{{ loading ? "loading..." : data }}
   </div>
